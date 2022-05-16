68b2d123e0aca7644937f39065b340eb8ae665d2
Bruno Figares <bfigares@tdtconsultants.com>
2022-05-16 15:30:45 -0300

    using refs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {config} from './config'
 
 const App = () => {
   const nodeEditor = React.useRef();
+  const inputState = React.useRef();
   const [nodes, setNodes] = React.useState({})
   const [conf,setConf] = React.useState(config);
 
@@ -13,11 +14,11 @@ const App = () => {
   const saveNodes = () => {
     const nodes = nodeEditor.current.getNodes()
 
-    var desc = document.querySelector('#loader').value = JSON.stringify(nodes)
+    inputState.current.value = JSON.stringify(nodes)
   }
   const loadNodes = () => {
     const nodes = nodeEditor.current.getNodes()
-    var desc = document.querySelector('#loader').value;
+    var desc = inputState.current.value;
     setNodes( desc? JSON.parse(desc):{})
   }
 
@@ -25,7 +26,7 @@ const App = () => {
     <div>
       <button onClick={saveNodes}>Save Logic</button>
       <button onClick={loadNodes}>Load Logic</button>
-      <input type='text' id='loader' name='nodosacargar'/>
+      <input ref={inputState} type='text' id='loader' name='nodosacargar'/>
 
       <div style={{width: 800, height: 600}}>
         { showNodeEditor &&
@@ -34,7 +35,6 @@ const App = () => {
           portTypes={conf.configurations['backend'].portTypes}
           nodeTypes={conf.configurations['backend'].nodeTypes}
           nodes={nodes}
-          onchange={setNodesInEditor}
         />}
       </div>
     </div>
